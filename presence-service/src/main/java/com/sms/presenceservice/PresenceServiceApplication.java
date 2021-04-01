c45e9e331db6871835713609aa4189f06fbc9604
ctrlcwho <ctrlcwho@protonmail.com>
2021-04-01 23:49:36 +0200

    SMS-131 Deploy the presences module + created a basic spring boot app + added a logging configuration + added a run.sh script + added a commit.sh script for haproxy configurations + added a health check endpoint + added authorization capabilities (excluding the health endpoint)

---
+++ presence-service/src/main/java/com/sms/presenceservice/PresenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sms.presenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresenceServiceApplication.class, args);
+    }
+
+}
