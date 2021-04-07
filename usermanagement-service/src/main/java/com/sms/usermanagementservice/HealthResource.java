7dd7f4848bb21ed91938752e333021d28373d83b
Mateusz Medoń <mmedon@student.agh.edu.pl>
2021-04-07 16:19:02 +0200

    SMS-137 User Management + user manegement module # haproxy.cfg

---
+++ usermanagement-service/src/main/java/com/sms/usermanagementservice/HealthResource.java
@@ -0,0 +1,16 @@
+package com.sms.usermanagementservice;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.core.Response;
+
+@RestController
+public class HealthResource {
+
+    @GetMapping("/health")
+    public Response health() {
+        return Response.ok().build();
+    }
+}
