d004e3db83cb25e46d183e8ec4988202c9a1f7ec
Angelika <angkubicka99@gmail.com>
2021-04-09 22:26:06 +0200

    Added scanBasePackages in main

--- grades-service/src/main/java/com/sms/gradesservice/GradesServiceApplication.java
+++ grades-service/src/main/java/com/sms/gradesservice/GradesServiceApplication.java
@@ -3,7 +3,7 @@ package com.sms.gradesservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sms")
 public class GradesServiceApplication {
 
     public static void main(String[] args) {
