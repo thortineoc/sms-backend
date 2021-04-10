606e0a1f88f8ba8aafd2502637262e6bdbb798a1
Mateusz Medoń <mmedon@student.agh.edu.pl>
2021-04-10 18:22:42 +0200

    SMS-137 # CR

---
+++ usermanagement-service/src/test/java/com/sms/usermanagementservice/UsermanagementServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sms.usermanagementservice;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
