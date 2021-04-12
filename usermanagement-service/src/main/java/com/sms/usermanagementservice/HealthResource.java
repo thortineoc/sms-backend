1d7c5a316fe3ab82b238b0fb3d7641d068e84781
ctrlcwho <ctrlcwho@protonmail.com>
2021-04-12 22:30:41 +0200

    SMS-151 Deploy a test react application + updated user-management-service # changed hardcoded client secred to a random UUID + fixed indentation in haproxy.cfg

--- usermanagement-service/src/main/java/com/sms/usermanagementservice/HealthResource.java
+++ usermanagement-service/src/main/java/com/sms/usermanagementservice/HealthResource.java
@@ -1,16 +1,14 @@
 package com.sms.usermanagementservice;
 
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.ws.rs.core.Response;
 
 @RestController
 public class HealthResource {
 
     @GetMapping("/health")
-    public Response health() {
-        return Response.ok().build();
+    public ResponseEntity<Object> health() {
+        return ResponseEntity.ok().build();
     }
 }
