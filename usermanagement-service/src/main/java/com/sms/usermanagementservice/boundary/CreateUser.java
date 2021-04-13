f4ea0c82ed514b2314a44246711d3a40b167d1b2
mateuszmulak <mateusz_mulak@icloud.com>
2021-04-13 18:47:55 +0200

    created endpoint for creating multiple users

--- usermanagement-service/src/main/java/com/sms/usermanagementservice/boundary/CreateUser.java
+++ usermanagement-service/src/main/java/com/sms/usermanagementservice/boundary/CreateUser.java
@@ -3,14 +3,27 @@ package com.sms.usermanagementservice.boundary;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Member;
+import java.util.List;
 
 @RestController
 @RequestMapping("/create-user")
 public class CreateUser {
 
     @PostMapping(path = "/new-user")
-    public String postBody(@RequestBody NewUserDTO data) {
-        return "Hello " + data.getUserFirstName() + data.getUserSecondName() + data.getUserSurname() + data.getToken();
+    public String newUser(@RequestBody NewUserDTO data) {
+        return "Hello " + data.getUserFirstName() + " " + data.getUserSecondName() + " " + data.getUserSurname() + " " + data.getToken();
+    }
+
+    @PostMapping(path = "/new-users")
+    public String newUsers(@RequestBody NewUsersDTO data) {
+        String output = "";
+        for(NewUserDTO user : data.getAllUsers()){
+            String str = "Hello " + user.getUserFirstName() + " " + user.getUserSecondName() + " " + user.getUserSurname() + " " + user.getToken();
+            output += str;
+            output += "\n";
+        }
+
+        return output;
     }
 
 }
