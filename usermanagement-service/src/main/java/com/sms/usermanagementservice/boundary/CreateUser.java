80c77890401a92b513a8fd96849cc2926e83138a
mateuszmulak <mateusz_mulak@icloud.com>
2021-04-14 19:25:54 +0200

    updated CreateUser

--- usermanagement-service/src/main/java/com/sms/usermanagementservice/boundary/CreateUser.java
+++ usermanagement-service/src/main/java/com/sms/usermanagementservice/boundary/CreateUser.java
@@ -1,26 +1,31 @@
 package com.sms.usermanagementservice.boundary;
 
+import com.sms.authlib.UserDTO;
+import com.sms.clients.KeycloakClient;
+import com.sms.usermanagementservice.control.UserMapper;
+import com.sms.usermanagementservice.entity.User;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/create")
 public class CreateUser {
 
     @PostMapping(path = "/new-user")
-    public String newUser(@RequestBody NewUserDTO data) {
-        return "Hello " + data.getUserFirstName() + " " + data.getUserSecondName() + " " + data.getUserSurname() + " " + data.getToken();
+    public String newUser(@RequestBody UserDTO data) {
+        User u = UserMapper.toUser(data);
+
+
+        return "Hello " + data.getFirstName();
+
     }
 
     @PostMapping(path = "/new-users")
-    public String newUsers(@RequestBody NewUsersDTO data) {
+    public String newUsers(@RequestBody List<UserDTO> users) {
         StringBuilder output = new StringBuilder();
-        for(NewUserDTO user : data.getAllUsers()){
-            String str = "Hello " + user.getUserFirstName() + " " + user.getUserSecondName() + " " + user.getUserSurname() + " " + user.getToken() + " ";
-            output.append(str);
-            if(user.getGroup().isPresent()){
-                output.append(user.getGroup().orElse(""));
-            }
-            output.append("\n");
+        for(UserDTO user : users){
+
         }
 
         return output.toString();
