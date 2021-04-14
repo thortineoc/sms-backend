8dd7f9d9bf04391b4e32c6b65d1fe281f8788cd4
mateuszmulak <mateusz_mulak@icloud.com>
2021-04-14 21:39:25 +0200

    updated CreateUser

--- usermanagement-service/src/main/java/com/sms/usermanagementservice/boundary/CreateUser.java
+++ usermanagement-service/src/main/java/com/sms/usermanagementservice/boundary/CreateUser.java
@@ -1,34 +1,34 @@
 package com.sms.usermanagementservice.boundary;
 
-import com.sms.authlib.UserDTO;
 import com.sms.clients.KeycloakClient;
+import com.sms.usermanagement.UserDTO;
 import com.sms.usermanagementservice.control.UserMapper;
 import com.sms.usermanagementservice.entity.User;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/create")
 public class CreateUser {
 
+    private final static KeycloakClient keyloakClient = new KeycloakClient();
+
     @PostMapping(path = "/new-user")
-    public String newUser(@RequestBody UserDTO data) {
-        User u = UserMapper.toUser(data);
+    public ResponseEntity<String> newUser(@RequestBody UserDTO data) {
+        User user = UserMapper.toUser(data);
+        String password = user.getFirstName().substring(0, Math.min(user.getFirstName().length(), 4)) +
+                user.getLastName().substring(0, Math.min(user.getLastName().length(), 4));
+        UserRepresentation userRepresentation = UserMapper.toUserRepresentation(user, password);
 
-
-        return "Hello " + data.getFirstName();
-
-    }
-
-    @PostMapping(path = "/new-users")
-    public String newUsers(@RequestBody List<UserDTO> users) {
-        StringBuilder output = new StringBuilder();
-        for(UserDTO user : users){
-
+        if(keyloakClient.createUser(userRepresentation)){
+            return new ResponseEntity<>("User Created!", HttpStatus.OK);
         }
 
-        return output.toString();
+        return new ResponseEntity<>("Cannot create user!", HttpStatus.BAD_REQUEST);
     }
 
 }
