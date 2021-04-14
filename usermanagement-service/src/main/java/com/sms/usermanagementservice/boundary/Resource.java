911b343b921feb7cd692fdf375bfa0a1198d0e8c
Mateusz Medoń <mmedon@student.agh.edu.pl>
2021-04-14 08:38:27 +0200

    SMS-170 endpoits-querying-users +add some endpoints

---
+++ usermanagement-service/src/main/java/com/sms/usermanagementservice/boundary/Resource.java
@@ -0,0 +1,47 @@
+package com.sms.usermanagementservice.boundary;
+
+import com.sms.usermanagementservice.control.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class Resource {
+
+    @Autowired
+    UsersService usersService;
+
+    @GetMapping("/")
+    public void allUsers(){
+        usersService.getAllUsers();
+    }
+
+    @GetMapping("/user/{id}")
+    @ResponseBody
+    public void user(@PathVariable("id") long id){
+        usersService.getUser(id);
+    }
+
+    @GetMapping("/usersid") //endpoint na wszytskie wartości id/ewentualnie potem tego gówna String
+    public void allUsersId(){
+        usersService.getAllUsersId();
+    }
+
+    @GetMapping("/group/{groupID}")
+    @ResponseBody
+    public void group(@PathVariable("groupID") short groupID){
+        usersService.getGroup(groupID);
+    }
+
+    @GetMapping("/teachers")
+    public void teachers(){
+        usersService.getTeachers();
+    }
+
+    @GetMapping("/teachers/{id}")
+    @ResponseBody
+    public void teacher(@PathVariable("id") short id){
+        usersService.getTeacher(id);
+    }
+
+}
---
+++ usermanagement-service/src/main/java/com/sms/usermanagementservice/control/UsersService.java
@@ -0,0 +1,13 @@
+package com.sms.usermanagementservice.control;
+
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("request")
+
+public class UsersService {
+
+
+}
