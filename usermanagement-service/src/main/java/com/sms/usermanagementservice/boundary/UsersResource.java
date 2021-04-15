13691fde39c97fbc7ee0d3bb3dfb67b3e4801281
mateuszmulak <mateusz_mulak@icloud.com>
2021-04-15 16:26:18 +0200

    added endpoint for creating teacher

--- usermanagement-service/src/main/java/com/sms/usermanagementservice/boundary/UsersResource.java
+++ usermanagement-service/src/main/java/com/sms/usermanagementservice/boundary/UsersResource.java
@@ -4,11 +4,9 @@ import com.sms.clients.KeycloakClient;
 import com.sms.context.UserContext;
 import com.sms.usermanagement.UserDTO;
 import com.sms.usermanagementservice.control.UserMapper;
-import com.sms.usermanagementservice.entity.User;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,13 +27,53 @@ public class UsersResource {
     private UserContext userContext;
 
     @PostMapping("/new-student")
-    public ResponseEntity<String> newUser(@RequestBody UserDTO data) {
+    public ResponseEntity<String> newStudent(@RequestBody UserDTO data) {
 
         if(!userContext.getSmsRole().equals("ADMIN")){
+            System.out.print("USER IS NOT AN ADMIN");
             return ResponseEntity.status(403).build();
         }
 
         if(data.getRole() != UserDTO.Role.STUDENT){
+            System.out.print("REQUEST DOES NOT CONTAINING STUDENT");
+            return ResponseEntity.badRequest().build();
+        }
+
+        if(!data.getCustomAttributes().getSubjects().isEmpty()){
+            System.out.print("DATA CONTAINING SUBJECTS");
+            return ResponseEntity.badRequest().build();
+        }
+
+        UserRepresentation userRepresentation = UserMapper.toUserRepresentation(data, usersService.calculatePassword(data.getFirstName(), data.getLastName()));
+        //todo create parent
+        if(keycloakClient.createUser(userRepresentation)){
+            return ResponseEntity.ok().build();
+        }
+
+        System.out.print("THERE WAS AN ERROR WHILE CREATING USER");
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/new-teacher")
+    public ResponseEntity<String> newTeacher(@RequestBody UserDTO data) {
+
+        if(!userContext.getSmsRole().equals("ADMIN")){
+            System.out.print("USER IS NOT AN ADMIN");
+            return ResponseEntity.status(403).build();
+        }
+
+        if(data.getRole() != UserDTO.Role.TEACHER){
+            System.out.print("REQUEST DOES NOT CONTAINING TEACHER");
+            return ResponseEntity.badRequest().build();
+        }
+
+        if(data.getCustomAttributes().getRelatedUser().isPresent()){
+            System.out.print("DATA CONTAINING RELATED USER");
+            return ResponseEntity.badRequest().build();
+        }
+
+        if(data.getCustomAttributes().getGroup().isPresent()){
+            System.out.print("DATA CONTAINING GROUP");
             return ResponseEntity.badRequest().build();
         }
 
@@ -45,6 +83,9 @@ public class UsersResource {
             return ResponseEntity.ok().build();
         }
 
+        System.out.print("THERE WAS AN ERROR WHILE CREATING USER");
         return ResponseEntity.badRequest().build();
     }
+
+
 }
