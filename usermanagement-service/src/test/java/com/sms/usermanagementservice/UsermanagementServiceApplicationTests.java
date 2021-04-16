6db0bb2dbc5fa8aed0ac3ce1c7de4a3bd87ad08c
mateuszmulak <mateusz_mulak@icloud.com>
2021-04-16 16:51:38 +0200

    refactor

--- usermanagement-service/src/test/java/com/sms/usermanagementservice/UsermanagementServiceApplicationTests.java
+++ usermanagement-service/src/test/java/com/sms/usermanagementservice/UsermanagementServiceApplicationTests.java
@@ -1,8 +1,7 @@
 package com.sms.usermanagementservice;
 
 
-import com.sms.usermanagementservice.boundary.UsersService;
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
