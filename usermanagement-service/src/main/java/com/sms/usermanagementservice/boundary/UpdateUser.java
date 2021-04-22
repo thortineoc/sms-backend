69571a784e71e1a352fd1960b5b2e8e74b7aa51f
EKubicka <ekubicka@student.agh.edu.pl>
2021-04-22 13:52:56 +0200

    Started class UpdateUser (Just starting, idk what I'm doing)

---
+++ usermanagement-service/src/main/java/com/sms/usermanagementservice/boundary/UpdateUser.java
@@ -0,0 +1,18 @@
+package com.sms.usermanagementservice.boundary;
+
+import com.sms.clients.KeycloakClient;
+import com.sms.usermanagement.UserDTO;
+import com.sms.usermanagementservice.control.UserMapper;
+import com.sms.usermanagementservice.entity.User;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequestMapping("/create")
+public class UpdateUser {
+
+}
