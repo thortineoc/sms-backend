1a89217d9c0fc89ba86a4dbee8225748c75fe513
EKubicka <ekubicka@student.agh.edu.pl>
2021-04-23 22:55:43 +0200

    Added shouldReturnForbiddenWhenNotAdmin test

---
+++ usermanagement-service/src/test/java/com/sms/usermanagementservice/UpdateUserTest.java
@@ -0,0 +1,47 @@
+package com.sms.tests.usermanagement;
+
+import com.google.common.collect.Lists;
+import com.sms.clients.KeycloakClient;
+import com.sms.clients.WebClient;
+import com.sms.clients.entity.UserSearchParams;
+import com.sms.usermanagement.CustomAttributesDTO;
+import com.sms.usermanagement.UserDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.http.HttpStatus;
+
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+public class UpdateUserTest {
+    private final static WebClient CLIENT = new WebClient("smsadmin", "smsadmin");
+    private final static KeycloakClient KEYCLOAK_CLIENT = new KeycloakClient();
+
+    @BeforeEach
+    public void cleanup() {
+        UserSearchParams params = new UserSearchParams().firstName("firstName");
+        List<UserRepresentation> createdUsers = KEYCLOAK_CLIENT.getUsers(params);
+
+        createdUsers.stream().map(UserRepresentation::getId).forEach(KEYCLOAK_CLIENT::deleteUser);
+    }
+
+    @Test
+    void shouldReturnForbiddenWhenNotAdmin() {
+
+        //GIVEN
+
+        WebClient tempWebClient = new WebClient();
+
+        UserDTO user = createUserDTO(UserDTO.Role.TEACHER);
+
+        //SHOULD RETURN FORBIDDEN WHEN USER IS NOT AN ADMIN
+        tempWebClient.request("usermanagement-service")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(user)
+                .post("/users/update")
+                .then()
+                .statusCode(HttpStatus.FORBIDDEN.value());
+    }
+}
