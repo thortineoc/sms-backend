91ace7f26a6a804bb1f4fcea5dfbddc7496f4e1f
mateuszmulak <mateusz_mulak@icloud.com>
2021-04-27 00:46:42 +0200

    [SMS-166] added getters add setters

--- grades-service/src/main/java/com/sms/gradesservice/grades/control/repository/GradeJPA.java
+++ grades-service/src/main/java/com/sms/gradesservice/grades/control/repository/GradeJPA.java
@@ -22,5 +22,51 @@ public class GradeJPA {
 
     private Integer weight;
 
+    public String getSubject() {
+        return subject;
+    }
 
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getTeacherId() {
+        return teacher_id;
+    }
+
+    public void setTeacherId(String teacher_id) {
+        this.teacher_id = teacher_id;
+    }
+
+    public String getStudentId() {
+        return student_id;
+    }
+
+    public void setStudentId(String student_id) {
+        this.student_id = student_id;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
 }
