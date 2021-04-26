a8d6c15c53987adf71aa6a26152307d4b5768ae1
mateuszmulak <mateusz_mulak@icloud.com>
2021-04-27 01:38:39 +0200

    [SMS-166] updated GradeRepository

--- grades-service/src/main/java/com/sms/gradesservice/grades/control/repository/GradeRepository.java
+++ grades-service/src/main/java/com/sms/gradesservice/grades/control/repository/GradeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.lang.NonNull;
 
 import javax.transaction.Transactional;
+import java.util.Collection;
 import java.util.List;
 
 @Transactional
@@ -11,4 +12,8 @@ public interface GradeRepository extends CrudRepository<GradeJPA, Integer>{
 
     @NonNull
     List<GradeJPA> findAll();
+
+    List<GradeJPA> findAllByStudentId(String studentId);
+
+    List<GradeJPA> findAllBySubjectAndStudentIdIn(String subject, Collection<String> studentIds);
 }
