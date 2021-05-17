70d2ecc906d23c0f75f63bd2aae66ebe2621b493
thortineoc <angkubicka99@gmail.com>
2021-05-17 11:54:04 +0200

    SMS-254 auth exception fix

--- homework-service/src/main/java/com/sms/homeworkservice/file/control/FileService.java
+++ homework-service/src/main/java/com/sms/homeworkservice/file/control/FileService.java
@@ -1,15 +1,14 @@
 package com.sms.homeworkservice.file.control;
+
 import com.sms.context.UserContext;
-import com.sms.model.homework.FileBaseJPA;
 import com.sms.model.homework.FileDetailJPA;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-
-import javax.naming.AuthenticationException;
+import org.springframework.web.server.ResponseStatusException;
 import javax.persistence.EntityNotFoundException;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Component
@@ -26,25 +25,13 @@ public class FileService {
         try {
             Optional<FileDetailJPA> file = fileRepository.findById(id);
             if (!userContext.getUserId().equals(file.get().getOwnerId())) {
-                throw new AuthenticationException(); //("This user is not the owner of the file  with ID: " + id + " cannot delete");
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN);
             }
             fileRepository.deleteById(id);
-            /*
-            int amount = fileRepository.deleteAllById(id);
-            if (amount == 0) {
-                throw new IllegalStateException("File with ID: " + id + " wasn't deleted");
-            } else if (amount > 1) {
-                throw new IllegalStateException("While deleting file with ID: " + id + " other files were deleted");
-            }
-            */
         } catch (ConstraintViolationException e) {
             throw new IllegalArgumentException("Deleting file: " + id + " violated database constraints: " + e.getConstraintName());
         } catch (EntityNotFoundException e) {
             throw new IllegalStateException("File with ID: " + id + " does not exist, can't delete");
-        } catch (AuthenticationException e) { //??????????
-            throw new IllegalStateException("This user is not the owner of the file  with ID: " + id + " cannot delete");
-        } catch (NoSuchElementException e) {
-            throw new IllegalStateException("Couldn't find file with ID: " + id);
         }
     }
 }
