c0b4b648475b4e84ff06b5ce6df129ae67b908f6
thortineoc <angkubicka99@gmail.com>
2021-05-18 17:24:36 +0200

    SMS-254 deleting file is working

--- homework-service/src/main/java/com/sms/homeworkservice/file/control/FileService.java
+++ homework-service/src/main/java/com/sms/homeworkservice/file/control/FileService.java
@@ -6,19 +6,24 @@ import com.sms.context.UserContext;
 import com.sms.homeworkservice.answer.control.AnswerRepository;
 import com.sms.homeworkservice.homework.control.HomeworkRepository;
 import com.sms.model.homework.FileJPA;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.persistence.EntityNotFoundException;
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 @Scope("request")
 public class FileService {
 
     @Autowired
-    FileRespository fileRespository;
+    FileRepository fileRepository;
 
     @Autowired
     UserContext userContext;
@@ -30,7 +35,7 @@ public class FileService {
     AnswerRepository answerRepository;
 
     public FileJPA getFile(Long id) {
-        return fileRespository.findById(id).orElseThrow(() -> new IllegalStateException("File " + id + " doesn't exist"));
+        return fileRepository.findById(id).orElseThrow(() -> new IllegalStateException("File " + id + " doesn't exist"));
     }
 
     public FileLinkDTO store(MultipartFile file, Long id, FileLinkDTO.Type type) throws IOException {
@@ -41,7 +46,7 @@ public class FileService {
             case HOMEWORK:  validateHomework(id);   break;
             default: throw new IllegalStateException("incorrect TYPE");
         }
-        return FileMapper.toDTO(fileRespository.save(FileDB));
+        return FileMapper.toDTO(fileRepository.save(FileDB));
     }
 
     private void validateAnswer(Long id) {
@@ -52,5 +57,23 @@ public class FileService {
     private void validateHomework(Long id) {
         if (userContext.getSmsRole() != UserDTO.Role.TEACHER) throw new IllegalStateException("Only teacher can add homework file");
         if (!homeworkRepository.existsById(id)) throw new IllegalStateException("Homework does not exists");
+    }
+
+    public void deleteFile(Long id) {
+        try {
+            Optional<FileJPA> file = fileRepository.findById(id);
+            if (file.isPresent()) {
+                if (!userContext.getUserId().equals(file.get().getOwnerId())) {
+                    throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+                }
+                fileRepository.deleteById(id);
+            } else {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+        } catch (ConstraintViolationException e) {
+            throw new IllegalArgumentException("Deleting file: " + id + " violated database constraints: " + e.getConstraintName());
+        } catch (EntityNotFoundException e) {
+            throw new IllegalStateException("File with ID: " + id + " does not exist, can't delete");
+        }
     }
 }
