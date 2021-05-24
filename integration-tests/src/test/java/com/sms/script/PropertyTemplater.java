84e4a12b09cac67a63b2455c9819c31a86451f7e
ctrlcwho <ctrlcwho@protonmail.com>
2021-05-25 00:42:35 +0200

    SMS-283 Config storage + copied PropertyTemplater to config  module

--- integration-tests/src/test/java/com/sms/script/PropertyTemplater.java
+++ integration-tests/src/test/java/com/sms/script/PropertyTemplater.java
@@ -1,41 +1,13 @@
 package com.sms.script;
 
+import com.sms.config.scripts.Templater;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Locale;
-import java.util.Properties;
 
 public class PropertyTemplater {
 
-    private final static String VALUES = "values.properties";
-    private final static String APP_PROPERTIES_TEMPLATE = "application.template.properties";
-    private final static String APP_PROPERTIES = "application.properties";
-
     public static void main(String[] args) throws IOException, URISyntaxException {
-        Properties values = new Properties();
-        values.load(PropertyTemplater.class.getClassLoader().getResourceAsStream(VALUES));
-
-        Path templatePath = Paths.get(PropertyTemplater.class
-                .getClassLoader()
-                .getResource(APP_PROPERTIES_TEMPLATE)
-                .toURI());
-        String template = new String(Files.readAllBytes(templatePath));
-
-        for (String value : values.stringPropertyNames()) {
-            template = template.replaceAll("\\{" + value + "\\}", values.getProperty(value));
-        }
-
-        Path outputPath = Paths.get("/", templatePath.subpath(0, templatePath.getNameCount() - 1).toString(), APP_PROPERTIES);
-        if (System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("windows")) {
-            outputPath = Paths.get(outputPath.toString().substring(1)).toAbsolutePath();
-        }
-
-        if (!Files.exists(outputPath)) {
-            Files.createFile(outputPath.toAbsolutePath());
-        }
-        Files.write(outputPath, template.getBytes());
+        Templater.template("application.properties");
     }
 }
