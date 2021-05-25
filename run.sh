856daa1101c75ffe4a8d41c64417370cc152d9fb
ctrlcwho <ctrlcwho@protonmail.com>
2021-05-25 14:31:32 +0200

    SMS-291 add lesson count to config # fixed timetable configs + added run, test and install scripts

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+case $1 in
+	start)
+	  cd docker && \
+    docker compose up && \
+    docker exec -i postgres pg_restore -U sms -v -d sms < db_dump_text
+		;;
+	stop)
+		;;
+	status)
+		;;
+	*)
+		echo "Invalid argument"
+		echo "Valid arguments: start|stop|status"
+		exit 1
+		;;
+esac
+
+./scripts/run-backend.sh grades-service $1
+./scripts/run-backend.sh homework-service $1
+./scripts/run-backend.sh presence-service $1
+./scripts/run-backend.sh timetable-service $1
+./scripts/run-backend.sh usermanagement-service $1
+
+
+
