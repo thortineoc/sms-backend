282ba0d8bece962719d3d30701333eaf986f9a77
Michal Stadryniak <stadryniak.michal@gmail.com>
2021-05-29 12:51:42 +0200

    sms-297 keycloak stop

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+PID=""
+running() {
+        PID=$(ps aux | grep "java.*keycloak" | grep -v "grep" | awk '{print $2}')
+        [[ $PID == "" ]]
+}
+
 case $1 in
 	start)
 	(cd docker/linux && docker-compose up) &
@@ -7,9 +13,14 @@ case $1 in
 	(cd docker && docker exec -i postgres psql -U sms -d sms < db_dump_text) &
 	#keycloak run
 	#setsid ./keycloak*/bin/standalone.sh -b0.0.0.0 -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=docker/keycloak_db.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING -Djboss.http.port=24080 -Djboss.management.http.port=24082 &#  > /dev/null 2>&1 < /dev/null &	
-	;;
+		;;
 	stop)
 	  (cd docker/linux && docker-compose down)
+	  if $(running); then
+                        echo "Keycloak is not running"
+	  else
+          	kill $PID
+	  fi
 		;;
 	status)
 		;;
