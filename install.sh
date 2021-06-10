da0873190800f1d8cef0a48327bb55068b40d43b
Michal Stadryniak <michal.stadryniak@gmail.com>
2021-06-10 23:54:21 +0200

    [SMS-299] updated db dump and scripts

--- install.sh
+++ install.sh
@@ -14,9 +14,7 @@ rm -f keycloak-12.0.4.tar.gz && \
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
 sudo chmod +x /usr/local/bin/docker-compose && \
 sed -i "s/^[#]*\s*WEB_HOST=.*/WEB_HOST=localhost/" config/scripts/values.properties && \
-sed -i "s/^[#]*\s*DATABASE_HOST=.*/DATABASE_HOST=localhost/" config/scripts/values.properties && \
-
-mvn clean install -Pbuild
+sed -i "s/^[#]*\s*DATABASE_HOST=.*/DATABASE_HOST=localhost/" config/scripts/values.properties
 
 #keycloak run
 #setsid ./keycloak*/bin/standalone.sh -b0.0.0.0 -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=docker/keycloak_db.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING -Djboss.http.port=24080 -Djboss.management.http.port=24082  > /dev/null 2>&1 < /dev/null &
